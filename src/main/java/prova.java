8fa9306f3fb26167c36a5155984ae050abca0688
fede <fedemelis1@gmail.com>
2022-03-05 19:09:30 +0100

    ciao

--- src/main/java/prova.java
+++ src/main/java/prova.java
@@ -1,9 +1,12 @@
 public class prova {
     public static void main(String[] args) {
+
         System.out.println("Ciao");
 
         int[] a = new int[10];
 
         short [] s = new short[7];
+
+        String p = "Ciao";
     }
 }
