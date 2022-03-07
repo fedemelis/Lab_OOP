00e6262ecfc1f2801cdebfc1c02462a3e4f90a32
fede <fedemelis1@gmail.com>
2022-03-07 21:56:15 +0100

    creazione classe MyPoint

---
+++ src/main/java/MyPoint.java
@@ -0,0 +1,54 @@
+public class MyPoint {
+    /*
+    *
+    * coordinate di un punto x e y
+    *
+    * */
+
+    int x;
+    int y;
+
+    /*
+    *
+    * posso creare un punto vuoto
+    * posso creare un punto a partire da due coordinate
+    * posso creare un punto a partire da un altro punto
+    *
+    * */
+
+    public MyPoint() {
+
+    }
+
+    public MyPoint(int xx, int yy) {
+        x = xx;
+        y = yy;
+    }
+
+    public MyPoint(MyPoint pp) {
+        x = pp.x;
+        y = pp.y;
+    }
+
+    /*
+    *
+    * swap di x e y
+    *
+    * */
+
+    public static void swap(MyPoint pp){
+        int tmp = pp.x;
+        pp.x = pp.y;
+        pp.y = tmp;
+    }
+
+    /*
+    *
+    * non funziona
+    *
+    * */
+    public static void changePoint(MyPoint p1, MyPoint p2){
+        p1 = p2;
+    }
+
+}
--- src/main/java/prova.java
+++ src/main/java/prova.java
@@ -1,12 +1,19 @@
+import java.util.Arrays;
+
 public class prova {
     public static void main(String[] args) {
 
-        System.out.println("Ciao");
+        MyPoint p1 = new MyPoint();
+        MyPoint p2 = new MyPoint();
 
-        int[] a = new int[10];
+        p1.x = 1;
+        p1.y = 2;
 
-        short [] s = new short[7];
+        p2.x = 10;
+        p2.y = 20;
 
-        String p = "Ciao";
+
+        MyPoint.swap(p1);
+
     }
 }
